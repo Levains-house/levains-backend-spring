853418af522462aabba3def53fb59a4d5f203abb
CokeLee777 <akak640@kyonggi.ac.kr>
2022-12-27 18:14:00 +0900

    불필요한 로직 제거

--- src/test/java/com/levainshouse/mendolong/controller/CommonDocControllerTest.java
+++ src/test/java/com/levainshouse/mendolong/controller/CommonDocControllerTest.java
@@ -1,6 +1,6 @@
 package com.levainshouse.mendolong.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.levainshouse.mendolong.RestDocsConfig;
 import com.levainshouse.mendolong.utils.ErrorRequest;
 import org.junit.jupiter.api.DisplayName;
@@ -11,58 +11,16 @@ import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDoc
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.restdocs.RestDocumentationExtension;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.web.servlet.MockMvc;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.levainshouse.mendolong.RestDocsConfig;
-import com.levainshouse.mendolong.dto.address.AddressRegisterRequest;
-import com.levainshouse.mendolong.dto.item.ItemRegisterRequest;
-import com.levainshouse.mendolong.dto.item.ItemUpdateTradeStatusRequest;
-import com.levainshouse.mendolong.dto.user.UserSignInRequest;
-import com.levainshouse.mendolong.entity.Item;
-import com.levainshouse.mendolong.entity.User;
-import com.levainshouse.mendolong.enums.ItemCategory;
-import com.levainshouse.mendolong.enums.ItemPurpose;
-import com.levainshouse.mendolong.enums.ItemTradeStatus;
-import com.levainshouse.mendolong.enums.UserRole;
-import com.levainshouse.mendolong.repository.ItemRepository;
-import com.levainshouse.mendolong.service.AddressService;
-import com.levainshouse.mendolong.service.ItemService;
-import com.levainshouse.mendolong.service.TokenService;
-import com.levainshouse.mendolong.service.UserService;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.restdocs.RestDocumentationExtension;
-import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
-import org.springframework.restdocs.payload.JsonFieldType;
-import org.springframework.restdocs.snippet.Attributes;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
-import static org.springframework.restdocs.payload.PayloadDocumentation.*;
-import static org.springframework.restdocs.request.RequestDocumentation.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @ActiveProfiles(value = "test")
 @SpringBootTest
