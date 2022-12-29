6121947e94f70bb4810a116d1293810d712e42f3
CokeLee777 <akak640@kyonggi.ac.kr>
2022-12-29 17:39:58 +0900

    revise health check logic

--- src/main/java/com/levainshouse/mendolong/controller/HealthCheckController.java
+++ src/main/java/com/levainshouse/mendolong/controller/HealthCheckController.java
@@ -1,16 +1,20 @@
 package com.levainshouse.mendolong.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/api/health-check")
-@RestController
+@Controller
 public class HealthCheckController {
 
     @GetMapping
-    public ResponseEntity<Void> healthCheck(){
-        return ResponseEntity.ok().build();
+    @ResponseStatus(HttpStatus.OK)
+    public String healthCheck(){
+        return "ok";
     }
 }
